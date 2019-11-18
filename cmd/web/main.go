ed2052c2b09034d7ee7d0767e44d4813da6c900e
Anton Fedyashov <anton.fedyashov@gmail.com>
2019-11-18 15:04:52 +0300

    Sleep before start

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"sync"
+	"time"
 
 	"github.com/DiaElectronics/online_kasse/cmd/web/api"
 	"github.com/DiaElectronics/online_kasse/cmd/web/app"
@@ -13,6 +14,8 @@ import (
 var log = structlog.New()
 
 func run(errc chan<- error) {
+	time.Sleep(time.Second * 10)
+
 	var mutex sync.Mutex
 	db, err := dal.NewPostgresDAL("kaznachey", "RfpyfxtqAF", "localhost:5432")
 	if err != nil {
