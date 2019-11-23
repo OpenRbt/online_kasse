49de8b2442b98da79724c3d751f13a2050770ffa
Anton Fedyashov <anton.fedyashov@gmail.com>
2019-11-23 15:04:30 +0300

    Driver code added

--- drivers/web_kasse/kasse.h
+++ drivers/web_kasse/kasse.h
@@ -1,11 +1,131 @@
 #ifndef _DIAE_KASSE_
 #define _DIAE_KASSE_
 #include <string>
+#include "libfptr10.h"
+
 class Kasse {
     public:
     // PrintReceipt must return "PRINTED" if no errors, or "FAIL:" if there are errors 
     std::string PrintReceipt(int sum, int card, int post) {
-        printf("receipt printed: sum: %d, is_card: %d, post: %d\n", sum, card, post);
+		printf("New receipt accepted by driver\n");
+
+		libfptr_handle fptr;
+		int errorCode;
+
+		libfptr_create(&fptr);
+
+		// Step 1: Configure connection to device
+		libfptr_set_single_setting(fptr, LIBFPTR_SETTING_PORT, std::to_wstring(LIBFPTR_PORT_USB).c_str());
+		libfptr_set_single_setting(fptr, LIBFPTR_SETTING_MODEL, LIBFPTR_MODEL_KAZNACHEY_FA);
+		libfptr_apply_single_settings(fptr);
+
+		// Step 2: Connect to device
+		libfptr_open(fptr);
+		bool isOpened = (libfptr_is_opened(fptr) != 0);
+		if (!isOpened) {
+			printf("USB connection to device failed\n");
+			libfptr_destroy(&fptr);
+			return "FAIL: CONNECTION FAILURE";
+		}
+		printf("Connection to device opened\n");
+
+		// Stage 3: Register the responsible person and log in
+		libfptr_set_param_str(fptr, 1021, L"Канатников А.В.");
+		libfptr_set_param_str(fptr, 1203, L"5401199801");
+		libfptr_operator_login(fptr);
+		if (errorCode != 0) {
+			printf("Operator login failure\n");
+			libfptr_close(fptr);
+			libfptr_destroy(&fptr);
+			return "FAIL: OPERATOR FAILURE";
+		}
+
+		// Stage 4: Check the shift
+		libfptr_open_shift(fptr);
+		errorCode = libfptr_error_code(fptr);
+
+		// If shift expired (was more than 24 hours long) - close it and open again
+		if (errorCode == 68 || errorCode == 141) {
+			printf("Shift expired - closing and reopening\n");
+
+			libfptr_set_param_str(fptr, LIBFPTR_PARAM_REPORT_TYPE, LIBFPTR_RT_CLOSE_SHIFT);
+			libfptr_report(fptr);
+			if (errorCode != 0) {
+				printf("Shift close failed\n");
+				libfptr_close(fptr);
+				libfptr_destroy(&fptr);
+				return "FAIL: SHIFT CLOSE FAILURE";
+			}
+
+			libfptr_open_shift(fptr);
+			if (errorCode != 0) {
+				printf("Shift open failed\n");
+				libfptr_close(fptr);
+				libfptr_destroy(&fptr);
+				return "FAIL: SHIFT OPEN FAILURE";
+			}
+		}
+
+		// Stage 5: Open receipt
+		libfptr_set_param_int(fptr, LIBFPTR_PARAM_RECEIPT_TYPE, LIBFPTR_RT_SELL);
+		libfptr_open_receipt(fptr);
+
+		// Stage 6: Register the service or commodity
+		libfptr_set_param_str(fptr, LIBFPTR_PARAM_COMMODITY_NAME, "Мойка автомобиля");
+		libfptr_set_param_double(fptr, LIBFPTR_PARAM_PRICE, double(sum));
+		libfptr_set_param_double(fptr, LIBFPTR_PARAM_QUANTITY, 1.0);
+		libfptr_set_param_int(fptr, LIBFPTR_PARAM_TAX_TYPE, LIBFPTR_TAX_NO);
+		libfptr_set_param_int(fptr, 1212, 4);
+		libfptr_set_param_int(fptr, 1214, 1);
+		libfptr_registration(fptr);
+
+		// Stage 7: Register total
+		libfptr_set_param_double(fptr, LIBFPTR_PARAM_SUM, double(sum));
+		libfptr_receipt_total(fptr);
+
+		// Stage 8: Set the payment method
+		if (card) {
+			libfptr_set_param_int(fptr, LIBFPTR_PARAM_PAYMENT_TYPE, LIBFPTR_PT_ELECTRONICALLY);
+		} else {
+			libfptr_set_param_int(fptr, LIBFPTR_PARAM_PAYMENT_TYPE, LIBFPTR_PT_CASH);
+		}
+		libfptr_set_param_double(fptr, LIBFPTR_PARAM_PAYMENT_SUM, double(sum));
+		libfptr_payment(fptr);
+
+		// Stage 9: Close the receipt
+		libfptr_close_receipt(fptr);
+		while (libfptr_check_document_closed(fptr) < 0) {
+			printf("Attempt to close document...\n");
+			continue;
+		}
+
+		// Stage 10: Check document
+		if (libfptr_get_param_bool(fptr, LIBFPTR_PARAM_DOCUMENT_CLOSED) == 0) {
+			libfptr_cancel_receipt(fptr);
+			printf("Document was not closed - receipt cancelled\n");
+			libfptr_close(fptr);
+			libfptr_destroy(&fptr);
+			return "FAIL: DOCUMENT CLOSE FAILED";
+		}
+
+		// Stage 11: Check the printing process
+		if (libfptr_get_param_bool(fptr, LIBFPTR_PARAM_DOCUMENT_PRINTED) == 0) {
+			while (libfptr_continue_print(fptr) < 0) {
+				printf("Attempt to print the document\n");
+				continue;
+			}
+		}
+
+		// Stage 12: Get data about last document
+		libfptr_set_param_int(fptr, LIBFPTR_PARAM_FN_DATA_TYPE, LIBFPTR_FNDT_LAST_DOCUMENT);
+		libfptr_fn_query_data(fptr);
+		int documentNumber = libfptr_get_param_int(fptr, LIBFPTR_PARAM_DOCUMENT_NUMBER);
+		printf("Receipt was registered with number %d\n", documentNumber);
+
+		libfptr_close(fptr);
+		libfptr_destroy(&fptr);
+
+        printf("Receipt printed: sum: %d, is_card: %d, post: %d\n", sum, card, post);
         return "PRINTED";
     }
 };
