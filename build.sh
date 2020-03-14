cfd98622e209eb07c2286568fd82b3d3d2a095ad
rcsiberia <rcsrambler@gmail.com>
2020-03-14 11:23:59 -0700

    bla

--- build.sh
+++ build.sh
@@ -4,9 +4,4 @@ cd cmd/web
 go build
 cd ..
 
-cd drivers/web_kasse
-./build.sh
-cp kasse.exe ../../cmd/web
-cd ../..
-
 ./deploy_kasse.sh
--- deploy_kasse.sh
+++ deploy_kasse.sh
@@ -4,10 +4,8 @@ function GetDIR {
   echo "$(pwd)" | sed 's/\//\\\//g'
 }
 
-cd ..
 CUR_DIR=$(GetDIR)
 echo "CUR_DIR is $CUR_DIR"
-cd install
 
 sed -i "s/{DIR}/$CUR_DIR/g" kasse.service
 sudo cp kasse.service /etc/systemd/system/
