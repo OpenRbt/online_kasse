bff8262a6459352f2211acfbba292debef561a01
Roman Fedyashov <roman.fedyashov@gmail.com>
2021-03-19 16:24:15 +0700

    deployment scripts fixed for ubuntu

--- deploy_kasse.sh
+++ deploy_kasse.sh
@@ -8,5 +8,6 @@ CUR_DIR=$(GetDIR)
 echo "CUR_DIR is $CUR_DIR"
 
 sed -i "s/{DIR}/$CUR_DIR/g" kasse.service
+sed -i "s/{USER}/$USER/g" kasse.service
 sudo cp kasse.service /etc/systemd/system/
 sudo systemctl enable kasse
